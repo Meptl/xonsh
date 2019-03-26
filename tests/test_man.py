2e0c86542937409b547eae8ce36b54e9cabeb110
Christopher Chin <christopher.chin@acquia.com>
2019-03-26 11:27:05 -0400

    Kay.

--- tests/test_man.py
+++ tests/test_man.py
@@ -1,41 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
-import shutil
-import xonsh
 
 from xonsh.completers.man import complete_from_man
 
 from tools import skip_if_on_windows
 
-XONSH_PREFIX = xonsh.__file__
-if "site-packages" in XONSH_PREFIX:
-    # must be installed version of xonsh
-    num_up = 5
-else:
-    # must be in source dir
-    num_up = 2
-for i in range(num_up):
-    XONSH_PREFIX = os.path.dirname(XONSH_PREFIX)
-PATH = (
-    os.path.join(os.path.dirname(__file__), "bin")
-    + os.pathsep
-    + os.path.join(XONSH_PREFIX, "bin")
-    + os.pathsep
-    + os.path.join(XONSH_PREFIX, "Scripts")
-    + os.pathsep
-    + os.path.join(XONSH_PREFIX, "scripts")
-    + os.pathsep
-    + os.path.dirname(sys.executable)
-    + os.pathsep
-    + os.environ["PATH"]
-)
-
-skip_if_no_man = pytest.mark.skipif(
-    shutil.which("man", path=PATH) is None, reason="man command not on PATH"
-)
 
 @skip_if_on_windows
-@skip_if_no_man
 def test_man_completion(monkeypatch, tmpdir, xonsh_builtins):
     tempdir = tmpdir.mkdir("test_man")
     monkeypatch.setitem(
